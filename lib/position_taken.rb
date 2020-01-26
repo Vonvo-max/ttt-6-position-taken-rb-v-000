7aea051163455d04357a27764e4da878af9d1d1f
Max Ringelheim <max.ringelheim@gmail.com>
2020-01-26 20:12:44 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -6,9 +6,7 @@ board = [" 0 ", " 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 "]
 def position_taken?(board, index)
  if board[index] = " " || board[index] = "" || board[index] = nil
    return false
- else
-   return true
- elsif board[index] == "X" || board[index] == "O" 
+ elsif board[index] == "X" || board[index] == "O"
    return true
  else
      return false
