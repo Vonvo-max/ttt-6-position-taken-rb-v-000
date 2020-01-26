2300a5e6b5cda2b1ebb52d88dca5298c2d417045
Max Ringelheim <max.ringelheim@gmail.com>
2020-01-26 18:38:11 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,6 +5,6 @@ board = [" 0 ", " 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 "]
 
 def position_taken?(board, index)
  if board[index] = " "
-   return false end
+   return false
  else return true end
 end
